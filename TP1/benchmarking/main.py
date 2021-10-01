09dae54b00d82cc0af780adf731440a7fb07b393
NathanRullier <nathan.rullier@gmail.com>
2021-10-01 19:03:13 -0400

    trying to make http call work with the threads, not going great rn

--- TP1/benchmarking/main.py
+++ TP1/benchmarking/main.py
@@ -1,11 +1,16 @@
 import logging
-
+from threading import *   
+import time
+import asyncio
+import aiohttp
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger("Benchmark App")
 
+sem = Semaphore(1)
+get_url = 'https://api.github.com'
 
 def main():
     logger.info("Starting benchmark")
@@ -14,7 +19,38 @@ def main():
     1000 GET requests sequentially.
     500 GET requests, then one minute sleep, followed by 1000 GET requests.
     """
+    y = Thread(target=send2, args=())
+    x = Thread(target=send1, args=())
+    y.start()
+    x.start()
 
+async def send1():
+    sem.acquire()
+    async with aiohttp.ClientSession() as session:
+        for i in range(10):
+            async with session.get(get_url) as resp:
+                answer = await resp.json()
+                print(answer)
+                print('message '+str(i)+' de thread 1')
+            sem.release()
 
+async def send2():
+    sem.acquire()
+    async with aiohttp.ClientSession() as session:
+        for i in range(10):
+            async with session.get(get_url) as resp:
+                answer = await resp.json()
+                print(answer)
+                print('message '+str(i)+' de thread 1')
+            sem.release()
+        time.sleep(60)
+        sem.acquire()
+        for i in range(10):
+            async with session.get(get_url) as resp:
+                answer = await resp.json()
+                print(answer)
+                print('message '+str(i)+' de thread 1')
+            sem.release()
+    
 if __name__ == '__main__':
     main()
