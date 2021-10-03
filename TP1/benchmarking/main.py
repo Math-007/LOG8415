a9187a2a4e5d6395ed7607e45b4c9306e6f68977
NathanRullier <nathan.rullier@gmail.com>
2021-10-03 15:29:57 -0400

    i think it works

--- TP1/benchmarking/main.py
+++ TP1/benchmarking/main.py
@@ -12,45 +12,52 @@ logger = logging.getLogger("Benchmark App")
 sem = Semaphore(1)
 get_url = 'https://api.github.com'
 
-def main():
+async def main():
     logger.info("Starting benchmark")
     """
     TODO : The container runs locally on your laptop and sends two separate threads:
     1000 GET requests sequentially.
     500 GET requests, then one minute sleep, followed by 1000 GET requests.
     """
-    y = Thread(target=send2, args=())
-    x = Thread(target=send1, args=())
-    y.start()
-    x.start()
+    # y = Thread(target=send2, args=())
+    # x = Thread(target=send1, args=())
+    # y.start()
+    # x.start()
+    await asyncio.gather(send1(), send2())
 
 async def send1():
-    sem.acquire()
+    # sem.acquire()
     async with aiohttp.ClientSession() as session:
-        for i in range(10):
+        for i in range(1000):
             async with session.get(get_url) as resp:
                 answer = await resp.json()
-                print(answer)
+                # print(answer)
                 print('message '+str(i)+' de thread 1')
-            sem.release()
+            # sem.release()
 
 async def send2():
-    sem.acquire()
+    # sem.acquire()
+    async with aiohttp.ClientSession() as session:
+        for i in range(500):
+            async with session.get(get_url) as resp:
+                answer = await resp.json()
+                # print(answer)
+                print('message '+str(i)+' de thread 2')
+            # sem.release()
+        time.sleep(60)
+        # sem.acquire()
+        for i in range(1000):
+            async with session.get(get_url) as resp:
+                answer = await resp.json()
+                # print(answer)
+                print('message '+str(i)+' de thread 2')
+            # sem.release()
+
+async def get():
     async with aiohttp.ClientSession() as session:
         for i in range(10):
             async with session.get(get_url) as resp:
                 answer = await resp.json()
-                print(answer)
-                print('message '+str(i)+' de thread 1')
-            sem.release()
-        time.sleep(60)
-        sem.acquire()
-        for i in range(10):
-            async with session.get(get_url) as resp:
-                answer = await resp.json()
-                print(answer)
-                print('message '+str(i)+' de thread 1')
-            sem.release()
-    
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
+
