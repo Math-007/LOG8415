3cbd9a94099ba16e6f6e0e9c2adbb3c131abbc05
Philippe Rivest <technophil98@gmail.com>
2021-10-03 22:18:07 -0400

    Configure Flask to repond everytime

--- TP1/app/app.py
+++ TP1/app/app.py
@@ -8,16 +8,19 @@ app = Flask(__name__)
 cluster_id = os.environ.get("CLUSTER_ID")
 instance_id = None
 
+# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-retrieval.html
 r = requests.get("http://169.254.169.254/latest/meta-data/instance-id")
 if (r.status_code == 200):
     instance_id = r.text
 
 @app.route('/')
-def index():
+@app.route('/<path:path>')
+def route(path="/"):
     return jsonify({
         "message": "Hello world !",
         "cluster": cluster_id,
-        "instance": instance_id
+        "instance": instance_id,
+        "path": path
     })
 
 
