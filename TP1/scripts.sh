9927337c5c48ab1206ea8cefb80e3dc36899ca44
Philippe Rivest <technophil98@gmail.com>
2021-10-10 23:28:58 -0400

    Added comments to scripts.sh

--- TP1/scripts.sh
+++ TP1/scripts.sh
@@ -4,9 +4,11 @@
 echo "Building docker container"
 docker build -t log8415/tp1-benchmarking:latest -f benchmarking/Dockerfile benchmarking
 
-echo "Running docker container"
+echo "Running docker container with benchmark program"
 docker run -it \
+    # Mount volume to save the graphs
     -v $(pwd)/benchmarking/img/:/app/img \
+    # Mount host system aws credentials
     -v $HOME/.aws/credentials:/root/.aws/credentials:ro \
     log8415/tp1-benchmarking:latest
 
