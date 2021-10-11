ab7700c8c95e0f80a6443d39ee5967c6e47dd35c
Philippe Rivest <technophil98@gmail.com>
2021-10-10 23:41:20 -0400

    Added error handling for flask app

--- TP1/app/app.py
+++ TP1/app/app.py
@@ -2,16 +2,20 @@ import os
 
 from flask import Flask, jsonify
 import requests
+import platform
 
 app = Flask(__name__)
 
-cluster_id = os.environ.get("CLUSTER_ID")
+cluster_id = os.environ.get("CLUSTER_ID", "unknown cluster")
 instance_id = None
 
-# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-retrieval.html
-r = requests.get("http://169.254.169.254/latest/meta-data/instance-id")
-if (r.status_code == 200):
-    instance_id = r.text
+try:
+    # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-retrieval.html
+    r = requests.get("http://169.254.169.254/latest/meta-data/instance-id", timeout=1)
+    if (r.status_code == 200):
+        instance_id = r.text
+except requests.ConnectionError:
+    instance_id = platform.node()
 
 @app.route('/')
 @app.route('/<path:path>')
