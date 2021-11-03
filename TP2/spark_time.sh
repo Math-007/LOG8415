5086d4d976c5e5ae2206be7df1ac238e90a5b281
Philippe Rivest <technophil98@gmail.com>
2021-11-03 15:19:32 -0400

    Add spark_time.sh

---
+++ TP2/spark_time.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# https://linuxapt.com/blog/754-install-and-configure-apache-spark-on-ubuntu-20-04
+
+# https://hadoop.apache.org/docs/current/hadoop-mapreduce-client/hadoop-mapreduce-client-core/MapReduceTutorial.html#Example:_WordCount_v1.0
+wordcount_jar="/opt/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.1.jar"
+
+for f in $1/*.txt; do
+	echo $f
+
+	for i in {1..3}; do
+		hdfs dfs -rm -f -r $1/output &>/dev/null
+		before=$(date +"%s.%N")
+		spark-submit $wordcount_jar wordcount -files $f $1 $1/output &>/dev/null
+		after=$(date +"%s.%N")
+
+		echo $after-$before | bc
+	done
+done
