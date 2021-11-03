396ae87fc0ccf75cdfe42417a60fbf9e629b017a
Philippe Rivest <technophil98@gmail.com>
2021-11-03 14:47:37 -0400

    Add hadoop_time.sh

--- TP2/linux_time.sh
+++ TP2/linux_time.sh
@@ -5,13 +5,11 @@ directory=$1
 for f in $1/*.txt; do
 	echo $f
 
-	for i in {1..3}
-	do
+	for i in {1..3}; do
 		before=$(date +"%s.%N")
 		cat $f | tr ' ' "\n" | sort | uniq -c > /dev/null
 		after=$(date +"%s.%N")
 
-		elapsed=$(echo $after-$before | bc)
-		echo $elapsed
+		echo $after-$before | bc
 	done
 done
